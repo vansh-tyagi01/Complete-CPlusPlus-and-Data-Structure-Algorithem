13fcc4c86fde6a18ac640ddfabba0f0727255308
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-12 07:47:08 +0530

    Array element access by index

---
+++ Arrays/Access_element_through_index.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10,20,30,40,50};
+    
+    // print element
+
+    cout<<arr[2]<<endl;
+    cout<<arr[0]<<endl;
+    cout<<arr[4]<<endl;
+}
