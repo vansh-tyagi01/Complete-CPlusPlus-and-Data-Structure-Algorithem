479ef02714ca6defc31e86d5581dcd9944618519
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-26 10:32:05 +0530

    Switch case for Number

---
+++ Switch_Case/switch_number.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int n = 1;
+
+    switch(n) {
+        case 1:
+                cout<<"First Number"<<endl;
+                    break;
+        case 2:
+                cout<<"second Number"<<endl;
+                    break;
+
+        default :
+                cout<<"It is default"<<endl;
+            }
+}
