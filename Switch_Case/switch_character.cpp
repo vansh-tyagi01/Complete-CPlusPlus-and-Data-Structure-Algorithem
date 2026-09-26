74cfb42053b3eeb47f9711c729b1f8ed9ab6e1e1
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-09-26 10:33:24 +0530

    Switch case for Character

---
+++ Switch_Case/switch_character.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int n = 'a';
+
+    switch(n) {
+        case 'a':
+                cout<<"First Number"<<endl;
+                    break;
+        case 'b':
+                cout<<"second Number"<<endl;
+                    break;
+
+        default :
+                cout<<"It is default"<<endl;
+            }
+}
